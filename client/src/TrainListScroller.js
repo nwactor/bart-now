307f12d30706d9ad8bbe854b3ce18cf8b521310a
nwactor <nickwactor@gmail.com>
2018-05-07 23:45:23 -0700

    adjust width of train list for phone screens

--- client/src/TrainListScroller.js
+++ client/src/TrainListScroller.js
@@ -4,16 +4,17 @@ import TrainPanel from "./TrainPanel";
 
 class TrainListScroller extends Component {
 	
+	//All of the 10s in parseInt are radixes, saying that we're parsing a base 10 int
 	sortTrains(trains) {
 		trains.sort((train1, train2) => {
-			var train1ETA = parseInt(parseInt(train1.minutes) + parseInt(train1.delay)); 
-			var train2ETA =	parseInt(parseInt(train2.minutes) + parseInt(train2.delay));
+			var train1ETA = parseInt(parseInt(train1.minutes, 10) + parseInt(train1.delay, 10), 10); 
+			var train2ETA =	parseInt(parseInt(train2.minutes, 10) + parseInt(train2.delay, 10), 10);
 
-			if(parseInt(train1ETA) === 'NaN' && parseInt(train2ETA) === 'NaN') {
+			if(parseInt(train1ETA, 10) === 'NaN' && parseInt(train2ETA, 10) === 'NaN') {
 		      	return 0;
-		    } else if(parseInt(train1ETA) === 'NaN') {
+		    } else if(parseInt(train1ETA, 10) === 'NaN') {
 		      	return -1; //1 is proably 'leaving' so it goes first
-		    } else if(parseInt(train2ETA) === 'NaN') {
+		    } else if(parseInt(train2ETA, 10) === 'NaN') {
 		      	return 1; //2 probably 'leaving'
 		    } else {
 		      	return Number(train1ETA) - Number(train2ETA); 
